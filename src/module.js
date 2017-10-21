5d5f9f0af7b93956d134d467f009dc16458a2346
chrisguttandin <chrisguttandin@media-codings.com>
2017-10-21 03:20:25 +0200

    blacklist imports of rxjs/add/*

--- src/module.js
+++ src/module.js
@@ -44,7 +44,7 @@ module.exports = {
         'directive-class-suffix': true,
         'eofline': true,
         'forin': true,
-        'import-blacklist': [ true, 'lodash-es', 'rxjs', 'rxjs/Rx' ],
+        'import-blacklist': [ true, 'lodash-es', 'rxjs', 'rxjs/add/*', 'rxjs/Rx' ],
         'import-destructuring-spacing': true,
         'import-spacing': true,
         'indent': [ true, 'spaces' ],
