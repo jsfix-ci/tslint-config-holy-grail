a73c0fae01e1d8ad10bf2436e0d355e4b8e945b0
Christoph Guttandin <chrisguttandin@media-codings.com>
2018-10-02 13:50:00 +0200

    feat: add rxjs-prefer-observer rule

--- src/module.js
+++ src/module.js
@@ -157,6 +157,7 @@ module.exports = {
         'rxjs-no-unsafe-first': true,
         'rxjs-no-unsafe-switchmap': true,
         'rxjs-no-unsafe-takeuntil': true,
+        'rxjs-prefer-observer': true,
         'rxjs-throw-error': true,
         'semicolon': [ true, 'always' ],
         'space-before-function-paren': true,
