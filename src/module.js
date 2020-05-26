a7eca2955d8cd5473d66e3653356a46c3dc961b5
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-05-27 00:59:25 +0200

    fix: add exceptions to invalid-void rule

--- src/module.js
+++ src/module.js
@@ -42,7 +42,7 @@ module.exports = {
         'indent': [ true, 'spaces' ],
         'interface-name': [ true, 'always-prefix' ],
         'interface-over-type-literal': true,
-        'invalid-void': [ true, { 'allow-generics': [ 'EventEmitter', 'Observable', 'Observer', 'Promise' ] } ],
+        'invalid-void': [ true, { 'allow-generics': [ 'EventEmitter', 'NextObserver', 'Observable', 'Observer', 'Promise', 'ReplaySubject', 'Subject' ] } ],
         'label-position': true,
         'linebreak-style': [ true, 'LF' ],
         'max-classes-per-file': [ true, 2 ],
