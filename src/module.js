861d33c17d4fffd23c055cc2eee97d43f42c5bb8
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-06-18 19:53:18 +0200

    fix: remove indent rule

--- src/module.js
+++ src/module.js
@@ -36,7 +36,6 @@ module.exports = {
         ],
         'import-destructuring-spacing': true,
         'increment-decrement': true,
-        'indent': [ true, 'spaces' ],
         'interface-name': [ true, 'always-prefix' ],
         'interface-over-type-literal': true,
         'invalid-void': [ true, { 'allow-generics': [ 'EventEmitter', 'NextObserver', 'Observable', 'Observer', 'Promise', 'ReplaySubject', 'Subject' ] } ],
